6fe5dceba961864126253084d4360b3fa82ea5d9
Megurian <eh202201078@wmsu.edu.ph>
2024-09-27 12:25:34 +0800

    addUser function

--- PHP/DB_Modules/account.class.php
+++ PHP/DB_Modules/account.class.php
@@ -2,15 +2,36 @@
 require_once 'database.php';
 
 class User {
-    public $username = '';
-    public $email = '';
-    public $first_name = '';
-    public $last_name = '';
-    public $password = '';
+    public $username = 'EH202201078';
+    public $email = 'eh202201078';
+    public $first_name = 'Meg Ryan';
+    public $last_name = 'Gomez';
+    public $password = 'MegRyanPH244';
 
     protected $db;
 
     function __construct() {
         $this->db = new Database();
     }
+
+    function addUser() {
+        $sql = "INSERT INTO user (username, email, first_name, last_name, password) VALUES (:username, :email, :first_name, :last_name, :password);";
+
+        $prepQuery = $this->db->connect()->prepare($sql);
+
+        $prepQuery->bindParam(':username', $this->username);
+        $prepQuery->bindParam(':email', $this->email);
+        $prepQuery->bindParam(':first_name', $this->first_name);
+        $prepQuery->bindParam(':last_name', $this->last_name);
+        $prepQuery->bindParam(':password', $this->password);
+
+        if($prepQuery->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
+/* $obj = new User();
+$obj->addUser(); */
